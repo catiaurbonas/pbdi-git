a17f284ce9b2745294b3c019940a2d26f24b0d48
Catia Urbonas <catia.urbonas@gmail.com>
2024-03-04 23:31:53 -0300

    exercicio01(git-python): Implementado Opacao Somar

---
+++ menu.py
@@ -0,0 +1,9 @@
+import calculadora
+def menu():
+    a = 1
+    b = 2
+    print("Menu","\n","1. Somar","\n","2. Subtrair","\n","3. Multiplicar","\n","4. Dividir", "\n","0. Sair")
+    opcao = int(input("Opção: "))
+    if opcao == 1:
+       soma = calculadora.somar(a,b)
+       print(f'{a} + {b} = {soma}')
